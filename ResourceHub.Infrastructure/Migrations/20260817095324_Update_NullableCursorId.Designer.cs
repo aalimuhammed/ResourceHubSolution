4db7dc330f8f0b854ea48fd97495d153950d7fd8
Shada-Adly <shada.adly@siac-construction.com>
2026-08-17 12:54:12 +0300

    - Update Nullable CursorId

--- ResourceHub.Infrastructure/Migrations/20260817095324_Update_NullableCursorId.Designer.cs
+++ ResourceHub.Infrastructure/Migrations/20260817095324_Update_NullableCursorId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ResourceHub.Infrastructure.Contexts;
 
@@ -11,9 +12,11 @@ using ResourceHub.Infrastructure.Contexts;
 namespace ResourceHub.Infrastructure.Migrations
 {
     [DbContext(typeof(ResourceHubDbContext))]
-    partial class ResourceHubDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260817095324_Update_NullableCursorId")]
+    partial class Update_NullableCursorId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ResourceHub.Infrastructure/Migrations/20260817095324_Update_NullableCursorId.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResourceHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_NullableCursorId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "cursor_id",
+                table: "services",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "cursor_id",
+                table: "services",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
